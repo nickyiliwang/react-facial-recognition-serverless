42f684f0118fc516535dbd920da280b025dde2d7
Nick Wang <nickyiliwang@gmail.com>
2019-11-11 10:51:17 -0500

    Updates

---
+++ precache-manifest.d4fa731e7ee5423e634bab90c98e574b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4de27db10724d11353533a8c6e2a3da6",
+    "url": "/react-facial-recognition-serverless-new/index.html"
+  },
+  {
+    "revision": "405de01b63283139348e",
+    "url": "/react-facial-recognition-serverless-new/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "2bfb2077e2402f4f38c2",
+    "url": "/react-facial-recognition-serverless-new/static/css/main.f1782b83.chunk.css"
+  },
+  {
+    "revision": "405de01b63283139348e",
+    "url": "/react-facial-recognition-serverless-new/static/js/2.24fafcf5.chunk.js"
+  },
+  {
+    "revision": "2bfb2077e2402f4f38c2",
+    "url": "/react-facial-recognition-serverless-new/static/js/main.13f7143b.chunk.js"
+  },
+  {
+    "revision": "0a82d9a26809cafbe0bc",
+    "url": "/react-facial-recognition-serverless-new/static/js/runtime~main.bb99efa8.js"
+  },
+  {
+    "revision": "7ae9c5eed534a12b90003b68f7b2f2c8",
+    "url": "/react-facial-recognition-serverless-new/static/media/face.7ae9c5ee.svg"
+  }
+]);
